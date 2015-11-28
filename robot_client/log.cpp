6037df62b0ae7a0ac3d03ea47f3bbc11dcf053b5
zhangshibo <453430198@qq.com>
2015-11-28 18:30:28 +0800

    add log4cplus lib to project.

---
+++ robot_client/log.cpp
@@ -0,0 +1,94 @@
+﻿#include "log.h"
+#include <sstream>
+#include <memory>
+#include <stdarg.h>
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/fileappender.h>
+#include <log4cplus/configurator.h>
+#include <log4cplus/configurator.h>
+
+using namespace std;
+using namespace log4cplus;
+using namespace log;
+
+static Logger global_pLogger;
+
+CLog* CLog::pInstance = NULL;
+
+CLog* CLog::Initialize(const string& strLogFile)
+{
+    if (NULL == pInstance)
+    {
+        pInstance = new CLog(strLogFile);
+    }
+    return pInstance;
+}
+
+CLog::CLog(const string& strLogFile)
+{
+    PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("./log4cplus.properties"));
+    global_pLogger = Logger::getRoot();
+}
+
+/*
+CLog::CLog(const string& strLogFile)
+{
+    //PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("/data/log/log4cplus.properties"));
+
+    //Logger global_pLogger = Logger::getRoot();
+
+    //定义一个文件Appender
+    SharedAppenderPtr oAppender(new FileAppender(LOG4CPLUS_TEXT(strLogFile)));
+
+    //Layout对象
+    string strPattern = "[%D{%Y-%m-%d %H:%M:%S}]%m%n";
+    auto_ptr<Layout> pLayout(new PatternLayout(strPattern));
+
+    //将Layout绑定到appender对象
+    oAppender->setLayout(pLayout);
+
+    //Logger对象
+    global_pLogger = Logger::getInstance(LOG4CPLUS_TEXT("LoggerName"));
+
+    //将需要appender对象绑定到到Logger对象上
+    global_pLogger.addAppender(oAppender);
+}
+*/
+
+void log::DEBUG_LOG(const std::string& strFile, int iLine, const char* format, ...)
+{
+    char logInfo[MAX_LINE];
+    va_list st;
+    va_start(st, format);
+    vsnprintf(logInfo, MAX_LINE, format, st);
+    stringstream ssLogData;
+    ssLogData << "[" << strFile << ":" << iLine << "] " << logInfo;
+    LOG4CPLUS_DEBUG(global_pLogger, ssLogData.str());
+    va_end(st);
+}
+
+void log::ERROR_LOG(const std::string& strFile, int iLine, const char* format, ...)
+{
+    char logInfo[MAX_LINE];
+    va_list st;
+    va_start(st, format);
+    vsnprintf(logInfo, MAX_LINE, format, st);
+    stringstream ssLogData;
+    ssLogData << "[" << strFile << ":" << iLine << "] " << logInfo;
+    LOG4CPLUS_ERROR(global_pLogger, ssLogData.str());
+    va_end(st);
+}
+
+void log::WARN_LOG(const std::string& strFile, int iLine, const char* format, ...)
+{
+    char logInfo[MAX_LINE];
+    va_list st;
+    va_start(st, format);
+    vsnprintf(logInfo, MAX_LINE, format, st);
+    stringstream ssLogData;
+    ssLogData << "[" << strFile << ":" << iLine << "] " << logInfo;
+    LOG4CPLUS_WARN(global_pLogger, ssLogData.str());
+    va_end(st);
+}
+
---
+++ robot_client/log.h
@@ -0,0 +1,31 @@
+﻿#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <string>
+#ifndef MAX_LINE
+#define MAX_LINE 1024
+#endif
+#define DEBUG(format, ...) log::DEBUG_LOG((__FILE__), (__LINE__), format, ## __VA_ARGS__);
+#define ERROR(format, ...) log::ERROR_LOG((__FILE__), (__LINE__), format, ## __VA_ARGS__);
+#define WARN(format, ...) log::WARN_LOG((__FILE__), (__LINE__), format, ## __VA_ARGS__);
+
+namespace log
+{
+
+void DEBUG_LOG(const std::string& strFile, int iLine, const char* format, ...);
+void ERROR_LOG(const std::string& strFile, int iLine, const char* format, ...);
+void WARN_LOG(const std::string& strFile, int iLine, const char* format, ...);
+
+class CLog
+{
+public:
+    static CLog* Initialize(const std::string& strLogFile);
+
+private:
+    CLog(const std::string& strLogFile);
+    static CLog* pInstance;
+};
+
+}
+#endif
+
